7b3c49ed29840b7b53080a39aa4c10138d0026ed
Gabriel Andrade <gabriel.andrade@smartsw.com.br>
2021-08-03 18:12:16 -0300

    Alterando codigo para deploy heroku

--- app.py
+++ app.py
@@ -1,6 +1,4 @@
 import datetime
-
-from bson import ObjectId
 from flask import Flask, render_template, request
 from pymongo import MongoClient
 
@@ -21,21 +19,6 @@ def create_app():
             entry_content = request.form.get("content")
             # entries.append((entry_content, formatted_date))
             app.db.entries.insert({"content": entry_content, "formatted_date": formatted_date})
-
-        entries_with_date = [
-            (
-                entry["_id"],
-                entry["content"],
-                entry["formatted_date"]
-            )
-            for entry in app.db.entries.find({})
-        ]
-        return render_template("home.html", entries=entries_with_date)
-
-    @app.route("/delete/<String:id>", methods=["DELETE"])
-    def home(id):
-        if request.method == "POST":
-            app.db.entries.delete_one({"_id": ObjectId(id)})
 
         entries_with_date = [
             (
